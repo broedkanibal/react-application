008d920cd1f72c27879d77634ddd20fabd12bc6e
Jesper Funk <funk.jesper@gmail.com>
2016-02-06 19:45:49 +0000

    Fix List bug, copied from tutorial

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,10 @@
-var React = require('require');
+var React = require('react');
 var ListItem = require('./ListItem.jsx');
 
 var List = React.createClass({
    render: function() {
        var createItem = function (text, index) {
-           return <ListItem key={index + text} text{text} />;
+           return <ListItem key={index + text} text={text} />;
        };
 
        return (<ul>{this.props.items.map(createItem)}</ul>);
